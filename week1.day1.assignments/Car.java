4e20475bb5b6aa95e7ee98c3aa14528ced4d8999
VigneshKumar <jarvis@bravetux.com>
2022-05-20 17:31:40 +0530

    Update Car.java
    Adding comments

--- week1.day1.assignments/Car.java
+++ week1.day1.assignments/Car.java
@@ -1,29 +1,34 @@
 package week1.day1.assignments;
 
 public class Car {
-	
+	//applyBreak Method
 	public void applyBreak() {
 		System.out.println("Apply Brake");
 	}
-
+	
+	//applyGear Method
 	public void applyGear() {
 		System.out.println("Apply Gear");
 	}
 	
+	//switchonAc Method
 	public void switchonAc() {
 		System.out.println("Switch on AC");
 	}
 
+	//applyAccelerate Method
 	public void applyAccelerate() {
 		System.out.println("Apply Acceleration");
 	}
 
+	//Main Function Invoke
 	public static void main(String[] args) {
+		
+		//Create Object MyCar for class Car
 		Car MyCar = new Car();
 		MyCar.applyAccelerate();
 		MyCar.applyGear();
 		MyCar.switchonAc();
 		MyCar.applyBreak();
 	}
-
 }
