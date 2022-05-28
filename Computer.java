397fc7b0ba82bdf303f67bdb2c1c91744c0ca667
VigneshKumar <jarvis@bravetux.com>
2022-05-28 22:29:24 +0530

    Add files via upload

---
+++ Computer.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package week3.day1.assignments.org.sys;
+
+/**
+ * @author vignesh
+ *
+ */
+public class Computer
+{
+
+	/**
+	 * Computer Method
+	 */
+	public void computerModel ( )
+	{
+		System.out.println("This is Computer Model");
+	}
+
+}
---
+++ Department.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package week3.day1.assignments.org.department;
+
+import week3.day1.assignments.org.college.College;
+
+/**
+ * @author vignesh
+ *
+ */
+public class Department extends College
+{
+
+	/**
+	 *  Department Methods
+	 */
+	public void deptName ( )
+	{
+		System.out.println("Computer Science Dept");
+	}
+
+}
